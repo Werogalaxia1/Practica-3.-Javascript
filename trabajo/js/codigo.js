68adb592b34f017ce6ed8ab615437c92bd61cbce
Werogalaxia1 <20213tn100@utez.edu.mx>
2022-06-06 10:06:43 -0500

    tarea_mauro

---
+++ trabajo/js/codigo.js
@@ -0,0 +1,65 @@
+let No, Ed,Lo,Pr;
+No=prompt("Escribir el Nombre, porfavor:")
+Ed=prompt("Escribir la Edad, porfavor:")
+Lo=prompt("Escribir la Localidad, porfavor:")
+Pr=prompt("Escribir la Profesión, porfavor:")
+
+
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<center>");
+document.write("<table border=3 ");
+document.write("<tr>");
+document.write("<td rowspan=4 width=30%  >");
+document.write("<img src=img/pe.jpg>");
+document.write("<center>");
+document.write ("<h1></h1>");
+document.write("</center>");
+document.write("</td>");
+document.write("<td >")
+document.write("Nombre:");
+document.write("</td>");
+document.write("<td>");
+document.write("",No);
+document.write("</td>");
+document.write("</tr>");
+document.write("<tr>");
+document.write("<td>");
+document.write("Edad:");
+document.write("</td>");
+document.write("<td>");
+document.write("",Ed);
+document.write("</td>");
+document.write("</tr>");
+document.write("<tr>");
+document.write("<td>");
+document.write("Localidad:");
+document.write("</td>");
+document.write("<td>");
+document.write("",Lo);
+document.write("</td>");
+document.write("</tr>");
+document.write("<tr>");
+document.write("<td>");
+document.write("Profesión:");
+document.write("</td>");
+document.write("<td>");
+document.write("",Pr);
+document.write("</td>");
+document.write(" </tr>")
+document.write("</table>");
+document.write("</center>");
